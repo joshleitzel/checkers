fd43a1b33a1f49ebc8758906af8bf4ee3b3e121b
Josh Leitzel <joshleitzel@gmail.com>
2012-02-13 17:21:29 -0500

    two

--- checkers/checkers.rb
+++ checkers/checkers.rb
@@ -12,7 +12,7 @@ def s(a, b)
 end
 
 LOGGING = false
-SHOW_EVERY_MOVE = false
+SHOW_EVERY_MOVE = true
 MOVE_LIMIT = 2 # a hard limit on the total number of moves EACH player can make
 MINIMAX_DEPTH = 3
 
@@ -62,7 +62,7 @@ def tree_from_state(s, rw, depth, k)
     if depth == 0
       break
     end
-    s = expand(s, rw, 0, count)
+    s = expand(s, rw == 0 ? 1 : 0, 0, count)
     if s != 0
       node << tree_from_state(s, rw, depth - 1, count)
     end
@@ -72,13 +72,13 @@ def tree_from_state(s, rw, depth, k)
   return node
 end
 
-def traverse(tree)
+def traverse(tree, rw)
   max = 0
   s_node = tree
   tree.children.each do |node|
-    traverse(node)
+    traverse(node, rw == 0 ? 1 : 0)
     s = node.content
-    v = board_value(s, 0)
+    v = rw == 1 ? board_value_one(s, rw) : board_value_two(s, rw)
     if v > max
       saved_s = s
       s_node = node
@@ -91,7 +91,6 @@ def traverse(tree)
   s_node.content
 end
 
-
 def simulate_game
   s = initialize_game
   
@@ -101,17 +100,38 @@ def simulate_game
   counter = 1
   for i in 1..MOVE_LIMIT
     if s != 0
-      s = traverse(tree_from_state(s, 0, MINIMAX_DEPTH, counter))
-      counter += 1
+      s = traverse(tree_from_state(s, 0, MINIMAX_DEPTH, counter), 0)
       last_s = s.clone unless s == 0
       
-      s = traverse(tree_from_state(s, 1, MINIMAX_DEPTH, counter))
+      if SHOW_EVERY_MOVE
+        print_board(s, counter)
+      end
       counter += 1
+
+      if s == 0
+        print bold("\nWhite wins!\n\n")
+      end
+      
+      s = traverse(tree_from_state(s, 1, MINIMAX_DEPTH, counter), 1)
       last_s = s.clone unless s == 0
+      
+      if SHOW_EVERY_MOVE
+        print_board(s, counter)
+      end
+      counter += 1
+      
+      if s == 0
+        print bold("\nRed wins!\n\n")
+      end
+      
       j = i
     end
   end
   
+  if j == MOVE_LIMIT && s != 0
+    print bold("\nMove limit reached before either player won.\n\n")
+  end
+    
   print "Total Moves: #{j * 2}\n\nFinal state:\n"
   print_board(last_s)
   
@@ -143,35 +163,35 @@ def initialize_game
       end
     end
   end
-  
-  s[[1, 2]] = 0
-  s[[1, 4]] = 0
-  s[[1, 6]] = 0
-  s[[2, 1]] = 0
-  s[[2, 3]] = 0
-  s[[2, 5]] = 0
-  s[[3, 2]] = 0
-  s[[3, 6]] = 0
-  s[[3, 8]] = 0
-  s[[3, 4]] = 3
-  s[[4, 3]] = 3
-  s[[2, 7]] = 3
-  s[[4, 7]] = 1
-  s[[5, 2]] = 4
-  s[[5, 8]] = 4
-  s[[7, 2]] = 4
-  s[[7, 4]] = 4
-  s[[8, 7]] = 4
-  s[[6, 1]] = 0
-  s[[6, 3]] = 0
-  s[[6, 5]] = 0
-  s[[6, 7]] = 0
-  s[[7, 6]] = 0
-  s[[7, 8]] = 0
-  s[[8, 1]] = 0
-  s[[8, 3]] = 0
-  s[[8, 5]] = 0
-  s[[1, 8]] = 0
+  # 
+  # s[[1, 2]] = 0
+  # s[[1, 4]] = 0
+  # s[[1, 6]] = 0
+  # s[[2, 1]] = 0
+  # s[[2, 3]] = 0
+  # s[[2, 5]] = 0
+  # s[[3, 2]] = 0
+  # s[[3, 6]] = 0
+  # s[[3, 8]] = 0
+  # s[[3, 4]] = 3
+  # s[[4, 3]] = 3
+  # s[[2, 7]] = 3
+  # s[[4, 7]] = 1
+  # s[[5, 2]] = 4
+  # s[[5, 8]] = 4
+  # s[[7, 2]] = 4
+  # s[[7, 4]] = 4
+  # s[[8, 7]] = 4
+  # s[[6, 1]] = 0
+  # s[[6, 3]] = 0
+  # s[[6, 5]] = 0
+  # s[[6, 7]] = 0
+  # s[[7, 6]] = 0
+  # s[[7, 8]] = 0
+  # s[[8, 1]] = 0
+  # s[[8, 3]] = 0
+  # s[[8, 5]] = 0
+  # s[[1, 8]] = 0
   s
 end
 
@@ -187,46 +207,41 @@ def move(s, from_x, from_y, to_x, to_y)
 end
 
 def expand(s, rw, move_num, pick_move_num)
+  last_move = 0
   list_of_moves = 0
   jump = 0
   
-#  while jump == 0
-    for a in 1..8
-      for b in 1..8
-        if (a + b) % 2 == 1
-          if (s[[a, b]] == 1 + rw) or (s[[a, b]] == 3 + rw)
-            result = exploit_moves(s, a, b, list_of_moves, jump, rw)
-            list_of_moves = result[:list_of_moves]
-            jump = result[:jump]
-          end
+  for a in 1..8
+    for b in 1..8
+      if (a + b) % 2 == 1
+        if (s[[a, b]] == 1 + rw) or (s[[a, b]] == 3 + rw)
+          result = exploit_moves(s, a, b, list_of_moves, jump, rw)
+          list_of_moves = result[:list_of_moves]
+          jump = result[:jump]
         end
       end
     end
-    
-    if jump == 0
-      if list_of_moves != 0
-        move = evaluate_moves(list_of_moves, pick_move_num)
-      else
-        s = 0
-        winner = rw == 0 ? 'White' : 'Red'
-        print bold("\n#{winner} wins!\n\n")
-      end
-    else  
+  end
+  
+  if jump == 0
+    #print "\njump 0"
+    if list_of_moves != 0
       move = evaluate_moves(list_of_moves, pick_move_num)
+    else
+      s = 0
     end
- # end
+  else
+    #print "\nno jump 0"
+    move = evaluate_moves(list_of_moves, pick_move_num)
+  end
   
   log("  > move: #{list_of_moves}")
   
-  if move == nil
+  if move == nil or move == 0
     s = 0
   end
   
   new_s = s != 0 ? simulate_move(s, move[0], move[1], move[2], move[3], rw) : 0
-  
-  if SHOW_EVERY_MOVE
-    print_board(new_s, move_num)
-  end
   
   new_s
 end
@@ -358,6 +373,9 @@ def simulate_move(s, from_x, from_y, to_x, to_y, rw)
 end
 
 def evaluate_moves(list_of_moves, num)
+  if num > list_of_moves.length - 1
+    return list_of_moves[num]
+  end
   list_of_moves[num]
 end
 
@@ -381,16 +399,40 @@ def minimax(s, rw, depth, v)
 end
 
 # Finds the value for a specific state of the board for a player
-def board_value(s, rw)
+def board_value_one(s, rw)
   value = 0
   for i in 1..8
     for j in 1..8
       if s[[i, j]] == rw + 1
         # regular pieces worh 1 point
         value = value + 1
+      elsif s[[i, j]] == rw + 2
+        value = value - 1
       elsif s[[i, j]] == rw + 3
         # kings worth 2 points
         value = value + 2
+      elsif s[[i, j]] == rw + 4
+        value = value - 2
+      end
+    end
+  end
+  value        
+end
+
+def board_value_two(s, rw)
+  value = 0
+  for i in 1..8
+    for j in 1..8
+      if s[[i, j]] == rw + 1
+        # regular pieces worh 1 point
+        value = value + 1
+      elsif s[[i, j]] == rw + 2
+        value = value - 1
+      elsif s[[i, j]] == rw + 3
+        # kings worth 2 points
+        value = value + 3
+      elsif s[[i, j]] == rw + 4
+        value = value - 3
       end
     end
   end
